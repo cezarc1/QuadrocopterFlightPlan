cb66c3650d50dabe34c11457a7f7006a21aea10c
ggamecrazy <me@cezarcocu.com>
2014-05-01 14:23:20 -0400

    Initial Commit

---
+++ QuadrocopterFlightPlan/QFPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QFPAppDelegate.h
+//  QuadrocopterFlightPlan
+//
+//  Created by Cezar Cocu on 5/1/14.
+//  Copyright (c) 2014 Cezar Cocu, Ahmed Shaikh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QFPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuadrocopterFlightPlan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuadrocopterFlightPlan
+//
+//  Created by Cezar Cocu on 5/1/14.
+//  Copyright (c) 2014 Cezar Cocu, Ahmed Shaikh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QFPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QFPAppDelegate class]));
+    }
+}
